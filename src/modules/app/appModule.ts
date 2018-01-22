6def3dbbe4b8df250a7d364e3937604b8f819e56
tuan.tran <test commit>
2018-01-22 17:02:27 +0700

    angular & type script part 1

---
+++ src/modules/app/appModule.ts
@@ -0,0 +1,13 @@
+import {NgModule} from "@angular/core";
+import {BrowserModule} from "@angular/platform-browser";
+import {DefaultPage} from "./defaultPage";
+
+@NgModule({
+    imports:[BrowserModule],
+    declarations:[DefaultPage],
+    bootstrap:[DefaultPage]
+})
+
+export class AppModule{
+    
+}
---
+++ src/modules/app/defaultPage.ts
@@ -0,0 +1,10 @@
+import {Component} from "@angular/core"
+
+@Component({
+    selector:"default-page",
+    template:"<h2>Tuan is studying angular & typescript</h2>"
+})
+
+export class DefaultPage{
+
+}
